f0f6d6a6b445aa69e0161216bb52b57a58cd2785
andreas <caternberg@gmx.de>
2023-10-16 11:41:51 +0200

    add

---
+++ JenkinsfileHelloWorld.groovy
@@ -0,0 +1,11 @@
+pipeline {
+    agent none
+    stages {
+        stage('Main') {
+            steps {
+                echo "Hello world"
+                sleep 10
+            }
+        }
+    }
+}
