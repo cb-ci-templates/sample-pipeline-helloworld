411f8d73cbc80a1291c33ba9031987dd64005fd7
cccaternberg <acaternberg@cloudbees.com>
2024-04-12 14:06:38 +0200

    curl changelog

--- JenkinsfileHelloWorld.groovy
+++ JenkinsfileHelloWorld.groovy
@@ -7,7 +7,8 @@ pipeline {
                 echo "Hello world"
                 withCredentials([string(credentialsId: 'jenkins-token', variable: 'jenkins-token')]) {
                     sh "curl -o changelog.xml https://${jenkins}@sda.acaternberg.flow-training.beescloud.com/sb/job/ci-templates-demo/job/testChangeLog/6/api/xml?wrapper=changes&xpath=//changeSet//comment"
-                    sh "cat changelog.xml "
+                    sh "cat changelog.xml"
+                    sh "env|sort"
                 }
             }
         }
