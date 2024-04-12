09abaf92a5f8ce6078c9185d4342d05ae4ce230a
cccaternberg <acaternberg@cloudbees.com>
2024-04-12 14:57:53 +0200

    add

--- JenkinsfileHelloWorld.groovy
+++ JenkinsfileHelloWorld.groovy
@@ -34,9 +34,9 @@ pipeline {
                 //see https://devops.stackexchange.com/questions/2310/get-all-change-logs-of-since-last-successful-build-in-jenkins-pipeline
                 script {
                     def changeLogSets = currentBuild.changeSets
-                    changeLogSets.each{it -> {
+                    changeLogSets.each{it ->
                         println it
-                    }}
+                    }
                 }
             }
         }
