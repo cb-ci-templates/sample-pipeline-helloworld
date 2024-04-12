9f3862ce3ab9b19c6a211ef14c58b6423250b7dd
cccaternberg <acaternberg@cloudbees.com>
2024-04-12 14:14:40 +0200

    add k8s agent

--- JenkinsfileHelloWorld.groovy
+++ JenkinsfileHelloWorld.groovy
@@ -1,5 +1,21 @@
 pipeline {
-    agent none
+    agent {
+        kubernetes {
+            yaml '''
+                apiVersion: v1
+                kind: Pod
+                spec:
+                  containers:
+                  - name: shell
+                    image: ubuntu
+                    command:
+                    - sleep
+                    args:
+                    - infinity
+                '''
+            defaultContainer 'shell'
+        }
+    }
 
     stages {
         stage('Main') {
