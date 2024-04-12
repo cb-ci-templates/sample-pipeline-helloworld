19e2aa5479421d34eae3bd830cd20747b002afc7
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2024-04-12 12:25:03 +0200

    Update JenkinsfileHelloWorld.groovy

--- JenkinsfileHelloWorld.groovy
+++ JenkinsfileHelloWorld.groovy
@@ -1,29 +1,10 @@
-/* REQUIRES MASTER EXECUTOR
-node {
-                sh """
-                    file="extended_choice_params.properties"
-                    if [ -e "\$file" ]; then
-                        rm -f "\$file"
-                        echo "File '\$file' deleted."
-                    fi    
-                    curl -O https://raw.githubusercontent.com/pipeline-demo-caternberg/pipeline-helloworld/main/\$file
-                    pwd && ls -l
-                    cat \$file
-                """
-}
- */
-
 pipeline {
     agent none
-    parameters {
-        extendedChoice multiSelectDelimiter: ',', name: 'myparam', propertyFile: './extended_choice_params.properties', propertyKey: 'mychoice_values', quoteValue: false, saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', visibleItemCount: 5
-    }
 
     stages {
         stage('Main') {
             steps {
                 echo "Hello world"
-                echo "${myparam}"
             }
         }
     }
