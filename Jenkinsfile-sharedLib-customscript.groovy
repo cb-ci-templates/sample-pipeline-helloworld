490492e86a8be49cd0baf129ca0eead3d23cb219
cccaternberg <acaternberg@cloudbees.com>
2024-04-17 11:49:31 +0200

    fix

--- Jenkinsfile-sharedLib-customscript.groovy
+++ Jenkinsfile-sharedLib-customscript.groovy
@@ -1,6 +1,7 @@
 library identifier: 'ci-shared-library@main', retriever: modernSCM(
         [$class: 'GitSCMSource',
          remote: 'https://github.com/cb-ci-templates/ci-shared-library.git'])
+def newSemanticVersionScript=""
 pipeline {
     agent {
         kubernetes {
@@ -23,6 +24,10 @@ pipeline {
     stages {
         stage('customStep') {
             steps {
+                script {
+                    newSemanticVersionScript = libraryResource 'scripts/newSemanticVersion.sh'
+                }
+                sh "echo ${newSemanticVersionScript} > test.sh && cat test.sh"
                 echo "#######################################################"
                 newSemanticVersion (arg:"-M",version:"1.2.3")
                 echo "${env.NEW_SEMANTIC_VERSION}"
