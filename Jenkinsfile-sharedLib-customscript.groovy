88eab7443745e8b08d785a507976b3f200d86ac2
cccaternberg <acaternberg@cloudbees.com>
2024-04-17 10:11:04 +0200

    add

---
+++ Jenkinsfile-sharedLib-customscript.groovy
@@ -0,0 +1,32 @@
+library identifier: 'ci-shared-library@main', retriever: modernSCM(
+        [$class: 'GitSCMSource',
+         remote: 'https://github.com/cb-ci-templates/ci-shared-library.git'])
+pipeline {
+    agent {
+        kubernetes {
+            yaml '''
+                apiVersion: v1
+                kind: Pod
+                spec:
+                  containers:
+                  - name: shell
+                    image: caternberg/ci-utils:1.0
+                    command:
+                    - sleep
+                    args:
+                    - infinity
+                '''
+            defaultContainer 'shell'
+        }
+    }
+
+    stages {
+        stage('customStep') {
+            steps {
+                echo "#######################################################"
+                echo "print commit messages"
+
+            }
+        }
+    }
+}
