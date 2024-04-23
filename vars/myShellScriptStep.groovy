aaf411303093bbc1778981077e40be49ef7b3615
cccaternberg <acaternberg@cloudbees.com>
2024-04-23 10:54:41 +0200

    add

--- vars/myShellScriptStep.groovy
+++ vars/myShellScriptStep.groovy
@@ -1,6 +1,6 @@
 def call(Map params){
     writeFile encoding: 'utf-8', file: "${WORKSPACE}/sampleFailFast.sh", text: libraryResource ('scripts/sampleFailFast.sh')
-    catchError(catchInterruptions: false,message: 'Pipeline Stage 1 failed with returnCode ${this.globalReturnCode}', buildResult:'FAILURE', stageResult: 'FAILURE') {
+    catchError(catchInterruptions: false,message: "Pipeline Stage 1 failed with returnCode ${env.globalReturnCode}", buildResult:'FAILURE', stageResult: 'FAILURE') {
         //Instead of using "script" step in a pipeline, we should better move it to a custom step in a shared Library.
         //We should better externalize the script. For demo purpose we make the script here inline:
         env.globalReturnCode=sh label: 'exitStatus',
