2fe2757c0279c8ac3ef22956e344c6a03319bd59
cccaternberg <acaternberg@cloudbees.com>
2024-04-23 12:30:46 +0200

    add

--- resources/scripts/sampleFailFast.sh
+++ resources/scripts/sampleFailFast.sh
@@ -4,7 +4,7 @@ set -u # Affects variables. When set, a reference to any variable you haven't pr
 #set -o pipefail # This setting prevents errors in a pipeline from being masked. If any command in a pipeline fails, that return code will be used as the return code of the whole pipeline.
 set -x # Enables a mode of the shell where all executed commands are printed to the terminal.
 RESULT_JSON=mytest.json
-echo $NonExistingVarCauseError
+#echo $NonExistingVarCauseError
 greetings="World"
 echo "{"hello": "$greetings"}" > ${WORKSPACE}/${RESULT_JSON}
 exit 0
