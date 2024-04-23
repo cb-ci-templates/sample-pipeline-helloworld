7f9f43dbb3e457b062bdaf1ca7b031ca14bf2165
cccaternberg <acaternberg@cloudbees.com>
2024-04-23 11:54:04 +0200

    add

--- vars/myShellScriptStep.groovy
+++ vars/myShellScriptStep.groovy
@@ -2,12 +2,12 @@ def returnCode="0"
 def call(Map params){
     catchError(catchInterruptions: false,message: "Pipeline Stage failed with returnCode ${returnCode}", buildResult:'FAILURE', stageResult: 'FAILURE') {
         writeFile encoding: 'utf-8', file: "${WORKSPACE}/sampleFailFast.sh", text: libraryResource ('scripts/sampleFailFast.sh')
-        env.globalReturnCode=sh label: 'exitStatus',
+        returnCode=sh label: 'exitStatus',
                 returnStatus: true,
                 script: """chmod a+x \${WORKSPACE}/sampleFailFast.sh && \
                             \${WORKSPACE}/sampleFailFast.sh
                          """
-        env.globalReturnCode=returnCode
+        env.globalReturnCode="${returnCode}"
         this.exitOrContinue()
         env.MYJSON=sh label: 'stdOut', returnStdout: true, script: "cat \${WORKSPACE}/mytest.json"
     }
@@ -16,7 +16,7 @@ def call(Map params){
 }
 
 def exitOrContinue(){
-    echo "call exitOrContinue  globalReturnCode:${returnCode}"
+    echo "call exitOrContinue  returnCode:${returnCode}"
     if ( returnCode != "0" ){
         sh "exit ${returnCode}"
     }
